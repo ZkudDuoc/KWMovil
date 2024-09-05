a4ff66c9b516ab77a9a8a54997bdf333825a228a
nico <nico.saavedram@duocuc.cl>
2024-09-05 15:41:24 -0400

    Login realizado con las validaciones, al logearse te redirije al home

--- QrAppKWM/src/app/pages/login/login.page.ts
+++ QrAppKWM/src/app/pages/login/login.page.ts
@@ -1,15 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage implements OnInit {
+export class LoginPage {
 
-  constructor() { }
+  loginFormulario: FormGroup;
 
-  ngOnInit() {
+  
+  usuarios = [
+    { username: 'Agustin', password: 'agustin' },
+    { username: 'Nicolas', password: 'nicolas' },
+    { username: 'Benja', password: 'benja' }
+  ];
+
+  constructor(private formBuilder: FormBuilder, private router: Router) {
+    
+    this.loginFormulario = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
   }
 
+  
+  validacionLogin() {
+    if (this.loginFormulario.valid) {
+      const username = this.loginFormulario.get('username')?.value;
+      const password = this.loginFormulario.get('password')?.value;
+  
+      
+      const usuarioValido = this.usuarios.find(user => user.username === username && user.password === password);
+  
+      if (usuarioValido) {
+        console.log('Inicio de sesión exitoso');
+        alert('Inicio de sesión exitoso');
+        
+        this.router.navigate(['/home']);
+      } else {
+        console.log('Credenciales incorrectas');
+        alert('Usuario o contraseña incorrectos');
+      }
+    } else {
+      alert('Por favor, completa todos los campos correctamente.');
+    }
+  }
 }
