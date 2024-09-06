137f586b1cae6b1a627cad317f367f4704d5cfef
unknown <Cetecom@SJAPCA03LC0821.duoc.acad>
2024-09-05 20:08:56 -0400

    Agregue mas datos al array, y el mensaje toast ya aparece cuando se logea, pero falta que se coloque el nombre de la variable

--- QrAppKWM/src/app/pages/login/login.page.ts
+++ QrAppKWM/src/app/pages/login/login.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -13,12 +14,14 @@ export class LoginPage {
 
   
   usuarios = [
-    { username: 'Agustin', password: 'agustin' },
-    { username: 'Nicolas', password: 'nicolas' },
-    { username: 'Benja', password: 'benja' }
+    { username: 'Agustin', password: 'agustin' , nombreCompleto: 'Agustin Cavieres', semestre: '4 semestre' , carrera: 'Informatica' , seccion: '008-V' , jornada: 'Vespertino'},
+    { username: 'Nicolas', password: 'nicolas' , nombreCompleto: 'Nicolas Saavedra', semestre: '2 semestre' , carrera: 'Mecanica' , seccion: '013-D' , jornada: 'Diurno'},
+    { username: 'Benja', password: 'benja' , nombreCompleto: 'Benjamin Morales', semestre: '8 semestre' , carrera: 'Medicina' , seccion: '007-V' , jornada: 'Vespertino'}
   ];
 
-  constructor(private formBuilder: FormBuilder, private router: Router) {
+  constructor(
+    private formBuilder: FormBuilder
+    , private router: Router, private toastController: ToastController) {
     
     this.loginFormulario = this.formBuilder.group({
       username: ['', Validators.required],
@@ -27,7 +30,7 @@ export class LoginPage {
   }
 
   
-  validacionLogin() {
+  async validacionLogin() {
     if (this.loginFormulario.valid) {
       const username = this.loginFormulario.get('username')?.value;
       const password = this.loginFormulario.get('password')?.value;
@@ -40,6 +43,15 @@ export class LoginPage {
         alert('Inicio de sesión exitoso');
         
         this.router.navigate(['/home']);
+        
+        const toast = await this.toastController.create({
+          message: 'Buenos dias, ${username}!',
+          duration: 2000,
+          position: 'top'
+        });
+        
+        toast.present();
+
       } else {
         console.log('Credenciales incorrectas');
         alert('Usuario o contraseña incorrectos');
