c4806edb37999b271dce6a1a3f8d675b298a6dbf
Zkud <nico.saavedram@duocuc.cl>
2024-09-22 18:44:23 -0300

    al fin funciono la camara ;)

--- QrAppKWM/src/app/pages/home/home.page.ts
+++ QrAppKWM/src/app/pages/home/home.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { UsuarioService } from '../../services/usuario.service';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';  // Importa Capacitor Camera
 
 @Component({
   selector: 'app-home',
@@ -9,6 +10,7 @@ import { UsuarioService } from '../../services/usuario.service';
 })
 export class HomePage implements OnInit {
   usuario: any = {}; // Inicializar `usuario` como un objeto vacío
+  capturedImage: any;  // Para almacenar la imagen capturada
 
   constructor(private router: Router, private usuarioService: UsuarioService) {
     const navigation = this.router.getCurrentNavigation();
@@ -23,6 +25,24 @@ export class HomePage implements OnInit {
     if (!this.usuario || !this.usuario.nombreCompleto) {
       // Como ejemplo, obtenemos el primer usuario de la lista del servicio
       this.usuario = this.usuarioService.getUsuarios()[0];  // Puedes cambiar esto según tu lógica
+    }
+  }
+
+  async openCamera() {
+    try {
+      const image = await Camera.getPhoto({
+        quality: 90,
+        allowEditing: false,
+        resultType: CameraResultType.Uri, // Puedes usar base64 si quieres el archivo en ese formato
+        source: CameraSource.Camera // Abre la cámara directamente
+      });
+
+      // Guarda la imagen capturada
+      this.capturedImage = image.webPath;
+      console.log('Imagen capturada:', this.capturedImage);
+
+    } catch (error) {
+      console.error('Error al capturar imagen', error);
     }
   }
 
